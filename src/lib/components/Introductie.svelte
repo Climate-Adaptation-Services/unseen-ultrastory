3fc3cfcea65332e6ba28f39d4ab00a3967284fbb
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2023-07-03 16:23:51 +0200

    j

--- src/lib/components/Introductie.svelte
+++ src/lib/components/Introductie.svelte
@@ -19,13 +19,7 @@
       name:'',
       image:'',
       time:0.2,
-      text:'Het is al drie dagen boven de 40°C in Eindhoven. De nachttemperatuur zakt ook niet meer onder de 25°C. Iedereen heeft het snikheet. We volgen het stel Leonie en Niels en hun zevenjarige zoon Sem. Niels is adviseur duurzaamheid bij de gemeente Eindhoven. Leonie werkt op de spoedeisende hulp van het Catharina Ziekenhuis.​'
-    },
-    {
-      name:'inbed',
-      image:'png',
-      time:0.3,
-      text:'Leonie en Niels slapen al nachten slecht omdat hun slaapkamer zo warm is.'
+      text:'Het is al drie dagen boven de 40°C in Eindhoven. De nachttemperatuur zakt ook niet meer onder de 25°C. Iedereen heeft het snikheet. We volgen het stel Leonie en Niels en hun zevenjarige zoon Sem. Niels is adviseur duurzaamheid bij de gemeente Eindhoven. Leonie werkt op de spoedeisende hulp van het Catharina Ziekenhuis. <br/><br/>Leonie en Niels slapen al nachten slecht omdat hun slaapkamer zo warm is.​'
     },
     {
       name:'sem',
@@ -53,7 +47,7 @@
 
   {#each scenes as scene,i}
     <div class='scroll-text-block' style='top:{`${(scene['time']+0.1)*stepSize}px`}'>
-      <p class='scroll-text'>{scene['text']}</p>
+      <p class='scroll-text'>{@html scene['text']}</p>
     </div>
   {/each}
 
