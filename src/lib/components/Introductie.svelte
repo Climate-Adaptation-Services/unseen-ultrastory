7d12cdeccdc8112a9219c1f18c204c6e70ed1eff
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2023-07-12 15:37:31 +0200

    nieuwe plaatjes

--- src/lib/components/Introductie.svelte
+++ src/lib/components/Introductie.svelte
@@ -16,7 +16,7 @@
 
   const scenes = [
     {
-      name:'bank',
+      name:'bank1',
       image:'png',
       time:0.2,
       text:'Het is begin juli en al drie dagen boven de 40°C in Eindhoven. De nachttemperatuur zakt ook niet meer onder de 25°C. Iedereen heeft het snikheet.<br/><br/> We volgen het stel Leonie en Niels en hun zevenjarige zoon Sem. Niels is leraar Nederlands. Leonie werkt op de spoedeisende hulp van het Catharina Ziekenhuis. <br/><br/>Leonie en Niels liggen uitgeput op de bank. Ze slapen al nachten slecht omdat hun slaapkamer zo warm is. Niels luistert naar het gezoem van de ventilator. Leonie denkt aan haar moeder die slecht ter been is in een appartement woont waar het niet uit te houden is.​'
@@ -53,6 +53,19 @@
   .stepdiv{
     height:100%;
   }
+
+  .item .image:after {
+  width: calc(100% + 150px);
+  height: 100px;
+  content: '';
+  position: absolute;
+  bottom: -125px;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 50%;
+  background-color: rgba(0,0,0,0.2);
+  filter: blur(20px);
+}
   
 </style>
 
