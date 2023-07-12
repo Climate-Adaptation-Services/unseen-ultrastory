d6bd64c3e45db7a2dc296dd93560cb6342ffc955
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2023-07-12 15:51:04 +0200

    plaatjes

--- src/lib/components/Introductie.svelte
+++ src/lib/components/Introductie.svelte
@@ -22,7 +22,7 @@
       text:'Het is begin juli en al drie dagen boven de 40°C in Eindhoven. De nachttemperatuur zakt ook niet meer onder de 25°C. Iedereen heeft het snikheet.<br/><br/> We volgen het stel Leonie en Niels en hun zevenjarige zoon Sem. Niels is leraar Nederlands. Leonie werkt op de spoedeisende hulp van het Catharina Ziekenhuis. <br/><br/>Leonie en Niels liggen uitgeput op de bank. Ze slapen al nachten slecht omdat hun slaapkamer zo warm is. Niels luistert naar het gezoem van de ventilator. Leonie denkt aan haar moeder die slecht ter been is in een appartement woont waar het niet uit te houden is.​'
     },
     {
-      name:'sem',
+      name:'seminbed',
       image:'png',
       time:0.5,
       text:'Sem kan niet slapen en is al dagen moe. De scholen zijn dicht vanwege de exceptionele hitte. Het is een geluk bij een ongeluk dat Niels leraar is: hij kan overdag voor Sem zorgen terwijl Leonie aan de slag moet op de spoedeisende hulp.  '
