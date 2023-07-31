5b49a057f3e6734bcf7e38ca98461c34276ba7af
sophievanderhorst17 <sophie_vanderhorst@hotmail.com>
2023-07-31 10:35:42 +0200

    leeslijst

--- src/lib/components/Aftiteling.svelte
+++ src/lib/components/Aftiteling.svelte
@@ -18,7 +18,7 @@
       name:'',
       image:'',
       time:0.1,
-      text:'De gebeurtenissen en personages in dit verhaal zijn verzonnen. Toch zou de extreme hitte in dit verhaal werkelijkheid kunnen worden. De laatste jaren zijn er overal in de wereld enorme uitschieters geweest aan extreme hitte, uitschieters die ook klimaatwetenschappers hebben verrast.'
+      text:'De laatste jaren zijn er overal in de wereld enorme uitschieters geweest aan extreme hitte, uitschieters die ook klimaatwetenschappers hebben verrast. Ook Nederland krijgt hier mogelijk mee te maken.'
     },
     {
       name:'',
@@ -30,13 +30,13 @@
       name:'',
       image:'',
       time:0.4,
-      text:'Dit verhaal is gebaseerd op de laatste wetenschappelijke inzichten over extreme weergebeurtenissen in Nederland en op talloze gesprekken met deskundigen op het gebied van klimaatadaptatie, hitte en crisismanagement.'
+      text:'Dit verhaal is gebaseerd op de laatste wetenschappelijke inzichten over exceptionele weergebeurtenissen in Nederland en op talloze gesprekken met deskundigen op het gebied van klimaatadaptatie, hitte en crisismanagement. De beelden zijn gemaakt met AI.'
     },
     {
       name:'',
       image:'',
       time:0.55,
-      text:'Onze dank gaat uit naar: Marije Bakker (NIPV), Lana Garrels (Veiligheidsregio Amsterdam Amstelland), Edwin Kok (NIPV), Frank van Lamoen (Provincie Noord-Brabant), Renee Linck (NIPV) en Brian Verhoeven (NIPV)'
+      text:'Onze dank gaat uit naar Marije Bakker (NIPV), Annelies Balkema (Waterschap De Dommel), Janette Bessembinder (KNMI), Lana Garrels (Veiligheidsregio Amsterdam Amstelland), Madeleen Helmer (Klimaatverbond Nederland), Edwin Kok (NIPV), Frank van Lamoen (Provincie Noord-Brabant), Renee Linck (NIPV), Karla Niggebrugge (Provincie Brabant), Brian Verhoeven (NIPV) en Karin van der Wiel (KNMI). '
     }
   ]
 
@@ -52,18 +52,29 @@
   <div class='stepdiv aftiteling'>
     <div class = 'leeslijst'>
       <h1>Meer lezen?</h1>
+      <h3 class = 'leeslijstcat'>Wetenschappelijke publicaties</h3>
       <ul class="myUL">
-        <li><a href="https://www.nature.com/articles/s41558-021-01092-9" target="_blank">Wetenschappelijk Artikel </a> over de toenemende kans op recordbrekende klimaatextremen</li>
-        <li>KNMI berichten:</li>
-          <ol style="list-style-type: circle;">
-            <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/ieder-jaar-een-verpletterend-hitterecord " target="_blank">Ieder jaar een verpletterend hitterecord?</a></li>
-            <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/absolute-temperatuurrecords-door-saharahitte " target="_blank">Absolute temperatuurrecords door Saharahitte</a></li>
-            <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/toename-hittegolven-in-europa-gelinkt-aan-veranderende-straalstroom" target="_blank">Toename hittegolven in Europa gelinkt aan veranderende straalstroom</a></li>
-          </ol>
-        <li><a href="https://www.ad.nl/binnenland/50-graden-in-nederland-moeten-rekening-houden-met-extreme-temperaturen~a9038ba2" target="_blank">50 graden in Nederland? </a></li>
-        <li><a href="https://www.youtube.com/watch?v=lhHbjdhAqxs" target="_blank">Filmpje: Krijgt Nederland steeds vaker te maken met hittegolven?</a></li>
-        <li><a href="https://www.youtube.com/watch?v=2NZYAkmqPr8" target="_blank">Filmpje: Temperatuur voor het eerst boven de 40 graden</a></li>
-        <li>Poster EGU</li>
+        <li><a href="https://www.nature.com/articles/s41558-021-01092-9" target="_blank">Artikel in Nature:</a> ‘Toenemende kans op recordbrekende klimaatextremen’
+        </li>
+        <li><a href="https://www.worldweatherattribution.org/reporting-extreme-weather-and-climate-change-a-guide-for-journalists/" target="_blank">Wetenschappelijke studie:</a> ‘Extreme hitte in Noord-Amerika, Europa en China in juli 2023 veel waarschijnlijker door klimaatverandering’ 
+        </li>
+        <li><a href="" target="_blank">Poster over gevolgen van exceptionele hitte</a>  
+        </li>
+      </ul>
+      
+      <h3 class = 'leeslijstcat'>KNMI berichten</h3>
+      <ul class="myUL">  
+        <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/ieder-jaar-een-verpletterend-hitterecord " target="_blank">Ieder jaar een verpletterend hitterecord?</a></li>
+        <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/absolute-temperatuurrecords-door-saharahitte " target="_blank">Absolute temperatuurrecords door Saharahitte</a></li>
+        <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/toename-hittegolven-in-europa-gelinkt-aan-veranderende-straalstroom" target="_blank">Toename hittegolven in Europa gelinkt aan veranderende straalstroom</a></li>
+        <li><a href="https://www.knmi.nl/over-het-knmi/nieuws/extreme-hitte-in-april-in-middellandse-zeegebied-waarschijnlijker-door-klimaatverandering" target="_blank">Extreme hitte in april in Middellandse Zeegebied waarschijnlijker door klimaatverandering</a></li>
+      </ul>
+      
+      <h3 class = 'leeslijstcat'>Video's en webinars</h3>
+      <ul class="myUL">
+        <li><a href="https://www.youtube.com/watch?v=lhHbjdhAqxs" target="_blank">Mini-college van Universiteit van Nederland: Krijgt Nederland steeds vaker te maken met hittegolven?</a></li>
+        <li><a href="https://www.youtube.com/watch?v=2NZYAkmqPr8" target="_blank">KNMI filmpje: Temperatuur voor het eerst boven de 40 graden</a></li>
+        <li><a href="https://klimaatverbond.nl/actueel/terugblik-op-het-webinar-de-hittelessen-van-portland-en-het-code-rood-scenario-in-nederland/" target="_blank">Webinar van het Klimaatverbond Nederland over de (hitte)lessen van Portland en het Code Rood-scenario in Nederland</a></li>
       </ul> 
     </div>
   </div>
@@ -75,6 +86,11 @@
 
   .stepdiv{
     height:100%;
+  }
+
+  .leeslijstcat{
+    text-align: left;
+    font-size: 2.5vh;
   }
 
   .stepdiv{
